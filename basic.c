ee102f85768744a9ca817681434373126ed9d0ee
stepin105337 <69156801+stepin105337@users.noreply.github.com>
2021-02-04 16:13:26 +0530

    Update basic

--- basic.c
+++ basic.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 
 int main()
+
 {
     
  printf("this is pradeep");
- 
- 
- printf("Hello, This is Tejaswini\n");
+ printf("Hello, This is Tejaswini\n"); 
+printf("\nhello im srinivas");
+printf("\nhello,this is harshitha");
+ printf("\nHello, This is Tejaswini\n");
  printf("Hello, this is hamsini");
  return 0;
 }
