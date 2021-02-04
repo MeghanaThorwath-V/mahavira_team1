6caae622bbc8908dff4e72e6f18bad7d7bcfb856
stepin105337 <69156801+stepin105337@users.noreply.github.com>
2021-02-04 17:17:57 +0530

    Updated

--- basic.c
+++ basic.c
@@ -1,15 +1,10 @@
 #include<stdio.h>
 
 int main()
-<<<<<<< HEAD
 
 {
     
  printf("this is pradeep");
- printf("Hello, This is Tejaswini\n"); 
-=======
-{  
->>>>>>> 0a486f7b533cbf7f14a16563ac76d8e53d4762ac
 printf("\nhello im srinivas");
 printf("\nhello,this is harshitha");
  printf("\nHello, This is Tejaswini\n");
