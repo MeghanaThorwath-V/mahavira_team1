d1115e5d27392718463668f9d97ccef33230305c
sathyavikas <69130833+sathyavikas@users.noreply.github.com>
2021-02-04 15:28:00 +0530

    Update basic.c

--- basic.c
+++ basic.c
@@ -2,24 +2,14 @@
 
 int main()
 {  
- printf("hello world");
- printf("This is Meghana Thorwath");
-    
- printf("hello world");
- printf("hello sathyavikas");
- printf("hai");
- printf("hello hamsini");
- printf("Hello, This is Tejaswini\n");
-
  printf("hello world\n");
+ printf("This is Meghana Thorwath\n");
+ printf("hello hamsini\n");
+ printf("Hello, This is Tejaswini\n");
  printf("hello sathyavikas\n");
- printf("hello pradeep\n");
- printf("\nhello world");
- printf("\nhello sathyavikas");
- printf("\nhello hamsini");
+ printf("hello pradeep");
  printf("\nHello..Rupa..!");
  printf("\nhello uzma kousar");
  printf("\nhello im srinivas");
-
  return 0;
 }
