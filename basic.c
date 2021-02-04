fd849fa37dcd9ca63b8086954f10cd5ba1d656a5
stepin105337 <69156801+stepin105337@users.noreply.github.com>
2021-02-04 13:50:01 +0530

    Update basic.c

--- basic.c
+++ basic.c
@@ -3,8 +3,9 @@
 int main()
 {
     
- printf("hello world");
- printf("hello sathyavikas");
- printf("hai");
+ printf("hello world\n");
+ printf("hello sathyavikas\n");
+ printf("hai\n");
+ printf("hello pradeep\n");
  return 0;
 }
