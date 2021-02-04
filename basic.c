e8e82466c2744b233a6a0a8ea0dccdf786abec4e
stepin105065 <69071665+stepin105065@users.noreply.github.com>
2021-02-04 15:19:41 +0530

    changed

--- basic.c
+++ basic.c
@@ -2,23 +2,6 @@
 
 int main()
 {
-<<<<<<< HEAD
-    
- printf("hello world");
- printf("hello sathyavikas");
- printf("hai");
- printf("hello hamsini");
  printf("Hello, This is Tejaswini");
-=======
- printf("hello world\n");
- printf("hello sathyavikas\n");
- printf("hello pradeep\n");
- printf("\nhello world");
- printf("\nhello sathyavikas");
- printf("\nhello hamsini");
- printf("\nHello..Rupa..!");
- printf("\nhello uzma kousar");
- printf("\nhello im srinivas");
->>>>>>> 6b2762d88a79502d4c3684bcde488b1dde6fa891
  return 0;
 }
